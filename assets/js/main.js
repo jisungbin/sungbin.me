0b6292079d0aa14de0c45a591b3742c4411b17ad
jisungbin <ji@sungb.in>
2021-03-31 14:58:18 +0900

    새로 만들자

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+(function ($) {
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500);
+        return false;
+    });
+
+    console.log('loaded js')
+})(jQuery);
